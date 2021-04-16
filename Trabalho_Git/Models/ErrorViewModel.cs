c99451091ad993807511b71d8813a6bc36a8500b
marcello abreu <marcellodeabreudasilva@gmail.com>
2021-04-16 20:14:02 -0300

    SubindoMVC

---
+++ Trabalho_Git/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trabalho_Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
